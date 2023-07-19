948ed2b57407482fed72d7f4d59507de41df86c4
Bowei Chen <bwchen.dev@gmail.com>
2023-07-20 00:27:53 +0800

    Export types from `Form`

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,4 +2,5 @@ import FormController from './FormController';
 import ErrorMessage from './components/ErrorMessage';
 import useForm from './useForm';
 
+export * from './types';
 export { FormController, ErrorMessage, useForm };
