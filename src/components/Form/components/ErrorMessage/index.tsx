962c67cab440fd14ebad8a56ba594b9544622da1
Bowei Chen <bwchen.dev@gmail.com>
2024-08-06 02:52:50 +0800

    Refactor ErrorMessage component to improve readability and maintainability

--- src/components/Form/components/ErrorMessage/index.tsx
+++ src/components/Form/components/ErrorMessage/index.tsx
@@ -1,23 +1,16 @@
 import { Group, Text, TextProps, useMantineTheme } from '@mantine/core';
 import { IconAlertCircle } from '@tabler/icons-react';
 
-export const ErrorMessage = (props: TextProps & { children?: string }) => {
+export function ErrorMessage(props: TextProps) {
   const theme = useMantineTheme();
   const { children, ...rest } = props;
-  if (!children?.length) return null;
-  return (
-    <Text
-      weight={500}
-      size="sm"
-      style={{ wordBreak: 'break-word', display: 'block', position: 'relative' }}
-      {...rest}
-    >
-      <Group spacing={5} sx={{ position: 'absolute' }}>
-        <IconAlertCircle width={theme.fontSizes.lg} />
-        {children}
-      </Group>
-    </Text>
-  );
-};
 
-export default ErrorMessage;
+  return (
+    <Group spacing={5} sx={{ position: 'absolute' }}>
+      <IconAlertCircle width={theme.fontSizes.lg} />
+      <Text weight={500} size="sm" style={{ wordBreak: 'break-word', display: 'block' }} {...rest}>
+        {children}
+      </Text>
+    </Group>
+  );
+}
